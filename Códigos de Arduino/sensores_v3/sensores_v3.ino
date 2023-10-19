94d911db2fac0365c6ffe858f42e5dfadcdd3141
JVHelion <45883508+JVHelion@users.noreply.github.com>
2023-10-19 10:01:24 -0300

    Notas e Código v2
    Adicionado o simulador do SIMUL IDE ao repositório.

--- Códigos de Arduino/sensores_v3/sensores_v3.ino
+++ Códigos de Arduino/sensores_v3/sensores_v3.ino
@@ -2,34 +2,70 @@
 #define SONAR_NUM 12 /* NÚMERO DE SENSORES */
 #define MAX_DISTANCE 200 /* DISTÂNCIA MÁXIMA DE 200 CM */
 
-const byte echo_sensor01 = 20;
-const byte triger_sensor01 = 21;
-const byte echo_sensor02 = 22;
-const byte triger_sensor02 = 23;
-const byte echo_sensor03 = 24;
-const byte triger_sensor03 = 25;
-const byte echo_sensor04 = 26;
-const byte triger_sensor04 = 27;
-const byte echo_sensor05 = 28;
-const byte triger_sensor05 = 29;
-const byte echo_sensor06 = 30;
-const byte triger_sensor06 = 31;
-const byte echo_sensor07 = 32;
-const byte triger_sensor07 = 33;
-const byte echo_sensor08 = 34;
-const byte triger_sensor08 = 35;
-const byte echo_sensor09 = 36;
-const byte triger_sensor09 = 37;
-const byte echo_sensor10 = 38;
-const byte triger_sensor10 = 39;
-const byte echo_sensor11 = 40;
-const byte triger_sensor11 = 41;
-const byte echo_sensor12 = 42;
-const byte triger_sensor12 = 43;
+/* DECLARAÇÃO DE CONSTANTES COMEÇO */
+  const byte echo_sensor01 = 20;
+  const byte trigger_sensor01 = 21;
+  const byte echo_sensor02 = 22;
+  const byte trigger_sensor02 = 23;
+  const byte echo_sensor03 = 24;
+  const byte trigger_sensor03 = 25;
+  const byte echo_sensor04 = 26;
+  const byte trigger_sensor04 = 27;
+  const byte echo_sensor05 = 28;
+  const byte trigger_sensor05 = 29;
+  const byte echo_sensor06 = 30;
+  const byte trigger_sensor06 = 31;
+  const byte echo_sensor07 = 32;
+  const byte trigger_sensor07 = 33;
+  const byte echo_sensor08 = 34;
+  const byte trigger_sensor08 = 35;
+  const byte echo_sensor09 = 36;
+  const byte trigger_sensor09 = 37;
+  const byte echo_sensor10 = 38;
+  const byte trigger_sensor10 = 39;
+  const byte echo_sensor11 = 40;
+  const byte trigger_sensor11 = 41;
+  const byte echo_sensor12 = 42;
+  const byte trigger_sensor12 = 43;
+/* DECLARAÇÃO DE CONSTANTES COMEÇO */
 
+/* ENTRADA E SAÍDA DE SENSORES */
+  NewPing sensor01(trigger_sensor01, echo_sensor01);
+  NewPing sensor02(trigger_sensor02, echo_sensor02);
+  NewPing sensor03(trigger_sensor03, echo_sensor03);
+  NewPing sensor04(trigger_sensor04, echo_sensor04);
+  NewPing sensor05(trigger_sensor05, echo_sensor05);
+  NewPing sensor06(trigger_sensor06, echo_sensor06);
+  NewPing sensor07(trigger_sensor07, echo_sensor07);
+  NewPing sensor08(trigger_sensor08, echo_sensor08);
+  NewPing sensor09(trigger_sensor09, echo_sensor09);
+  NewPing sensor10(trigger_sensor10, echo_sensor10);
+  NewPing sensor11(trigger_sensor11, echo_sensor11);
+  NewPing sensor12(trigger_sensor12, echo_sensor12);
+/* ENTRADA E SAÍDA DE SENSORES FIM */
 
 /* Armazena a quantidade de vezes que a medição deve ocorrer, para cada sensor */
 unsigned long pingTimer[SONAR_NUM]; 
 
 /* Armazena o número de medições */
 unsigned int cm[SONAR_NUM]; 
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+/* Use as instâncias da classe NewPing para medir as distâncias */
+  int distance01 = sensor01.ping_cm();
+  int distance02 = sensor02.ping_cm();
+  int distance03 = sensor03.ping_cm();
+  int distance04 = sensor04.ping_cm();
+  int distance05 = sensor05.ping_cm();
+  int distance06 = sensor06.ping_cm();
+  int distance07 = sensor07.ping_cm();
+  int distance08 = sensor08.ping_cm();
+  int distance09 = sensor09.ping_cm();
+  int distance10 = sensor10.ping_cm();
+  int distance11 = sensor11.ping_cm();
+  int distance12 = sensor12.ping_cm();
+}
