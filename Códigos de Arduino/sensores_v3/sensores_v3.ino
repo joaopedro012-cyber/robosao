8dbca0bcba063fed0e3dcbf26f9424d7bdf7e246
JVHelion <45883508+JVHelion@users.noreply.github.com>
2023-10-30 08:15:56 -0300

    Notas e Código v2.2

--- Códigos de Arduino/sensores_v3/sensores_v3.ino
+++ Códigos de Arduino/sensores_v3/sensores_v3.ino
@@ -1,47 +1,52 @@
+
+ /* CÓDIGO FUNCIONAL */
+
 #include <NewPing.h>
+#include <SD.h>
+
 #define SONAR_NUM 12 /* NÚMERO DE SENSORES */
-#define MAX_DISTANCE 200 /* DISTÂNCIA MÁXIMA DE 200 CM */
+#define MAX_DISTANCE 300 /* DISTÂNCIA MÁXIMA DE 200 CM */
 
 /* DECLARAÇÃO DE CONSTANTES COMEÇO */
-  const byte echo_sensor01 = 20;
-  const byte trigger_sensor01 = 21;
-  const byte echo_sensor02 = 22;
-  const byte trigger_sensor02 = 23;
-  const byte echo_sensor03 = 24;
-  const byte trigger_sensor03 = 25;
-  const byte echo_sensor04 = 26;
-  const byte trigger_sensor04 = 27;
-  const byte echo_sensor05 = 28;
-  const byte trigger_sensor05 = 29;
-  const byte echo_sensor06 = 30;
-  const byte trigger_sensor06 = 31;
-  const byte echo_sensor07 = 32;
-  const byte trigger_sensor07 = 33;
-  const byte echo_sensor08 = 34;
-  const byte trigger_sensor08 = 35;
-  const byte echo_sensor09 = 36;
-  const byte trigger_sensor09 = 37;
-  const byte echo_sensor10 = 38;
-  const byte trigger_sensor10 = 39;
-  const byte echo_sensor11 = 40;
-  const byte trigger_sensor11 = 41;
-  const byte echo_sensor12 = 42;
-  const byte trigger_sensor12 = 43;
+  #define echo_sensor01  44
+  #define trigger_sensor01  45
+  #define echo_sensor02  22
+  #define trigger_sensor02  23
+  #define echo_sensor03  24
+  #define trigger_sensor03  25
+  #define echo_sensor04  26
+  #define trigger_sensor04  27
+  #define echo_sensor05  28
+  #define trigger_sensor05  29
+  #define echo_sensor06  30
+  #define trigger_sensor06  31
+  #define echo_sensor07  32
+  #define trigger_sensor07  33
+  #define echo_sensor08  34
+  #define trigger_sensor08  35
+  #define echo_sensor09  36
+  #define trigger_sensor09  37
+  #define echo_sensor10  38
+  #define trigger_sensor10  39
+  #define echo_sensor11  40
+  #define trigger_sensor11  41
+  #define echo_sensor12  42
+  #define trigger_sensor12  43
 /* DECLARAÇÃO DE CONSTANTES COMEÇO */
 
 /* ENTRADA E SAÍDA DE SENSORES */
-  NewPing sensor01(trigger_sensor01, echo_sensor01);
-  NewPing sensor02(trigger_sensor02, echo_sensor02);
-  NewPing sensor03(trigger_sensor03, echo_sensor03);
-  NewPing sensor04(trigger_sensor04, echo_sensor04);
-  NewPing sensor05(trigger_sensor05, echo_sensor05);
-  NewPing sensor06(trigger_sensor06, echo_sensor06);
-  NewPing sensor07(trigger_sensor07, echo_sensor07);
-  NewPing sensor08(trigger_sensor08, echo_sensor08);
-  NewPing sensor09(trigger_sensor09, echo_sensor09);
-  NewPing sensor10(trigger_sensor10, echo_sensor10);
-  NewPing sensor11(trigger_sensor11, echo_sensor11);
-  NewPing sensor12(trigger_sensor12, echo_sensor12);
+  NewPing sensor01(trigger_sensor01, echo_sensor01, 200);
+  NewPing sensor02(trigger_sensor02, echo_sensor02, 200);
+  NewPing sensor03(trigger_sensor03, echo_sensor03, 200);
+  NewPing sensor04(trigger_sensor04, echo_sensor04, 200);
+  NewPing sensor05(trigger_sensor05, echo_sensor05, 200);
+  NewPing sensor06(trigger_sensor06, echo_sensor06, 200);
+  NewPing sensor07(trigger_sensor07, echo_sensor07, 200);
+  NewPing sensor08(trigger_sensor08, echo_sensor08, 200);
+  NewPing sensor09(trigger_sensor09, echo_sensor09, 200);
+  NewPing sensor10(trigger_sensor10, echo_sensor10, 200);
+  NewPing sensor11(trigger_sensor11, echo_sensor11, 200);
+  NewPing sensor12(trigger_sensor12, echo_sensor12, 200);
 /* ENTRADA E SAÍDA DE SENSORES FIM */
 
 void setup() {
@@ -63,29 +68,29 @@ void loop() {
   int distance11 = sensor11.ping_cm();
   int distance12 = sensor12.ping_cm();
 
-  Serial.println("distance01");
-  Serial.println(sensor12.ping_cm());
-  Serial.println("distance02");
+  Serial.print("A distancia do sensor 01:");
+  Serial.println(distance01);
+  Serial.print("A distancia do sensor 02:");
   Serial.println(distance02);
-  Serial.println("distance03");
+  Serial.print("A distancia do sensor 03:");
   Serial.println(distance03);
-  Serial.println("distance04");
+  Serial.print("A distancia do sensor 04:");
   Serial.println(distance04);
-  Serial.println("distance05");
+  Serial.print("A distancia do sensor 05:");
   Serial.println(distance05);
-  Serial.println("distance06");
+  Serial.print("A distancia do sensor 06:");
   Serial.println(distance06);
-  Serial.println("distance07");
+  Serial.print("A distancia do sensor 07:");
   Serial.println(distance07);
-  Serial.println("distance08");
+  Serial.print("A distancia do sensor 08:");
   Serial.println(distance08);
-  Serial.println("distance09");
+  Serial.print("A distancia do sensor 09:");
   Serial.println(distance09);
-  Serial.println("distance10");
+  Serial.print("A distancia do sensor 10:");
   Serial.println(distance10);
-  Serial.println("distance11");
+  Serial.print("A distancia do sensor 11:");
   Serial.println(distance11);
-  Serial.println("distance12");
+  Serial.print("A distancia do sensor 12:");
   Serial.println(distance12);
   delay(10000);
 }
---
+++ Códigos de Arduino/sensores_v4/sensores_v4.ino
@@ -0,0 +1,19 @@
+#include <NewPing.h>
+
+#define MAX_DISTANCE 350
+#define TRIGGER_PIN1  22
+#define ECHO_PIN1     23
+
+
+NewPing sonar(TRIGGER_PIN1, ECHO_PIN1, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+
+void setup() {
+  Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
+}
+
+void loop() {
+  delay(50);                     // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+  Serial.print("Ping: ");
+  Serial.print(sonar.ping_cm()); // Send ping, get distance in cm and print result (0 = outside set distance range)
+  Serial.println("cm");
+}
