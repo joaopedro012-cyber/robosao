2d1e9d8439467ba2d87acae91eddb8c47ba1efd0
JVHelion <45883508+JVHelion@users.noreply.github.com>
2023-11-03 09:20:48 -0300

    Notas e Código v2.5.1
    O código continua com mesma funcionalidade porém otimizado.

--- Códigos de Arduino/sensores_v3/sensores_v3.ino
+++ Códigos de Arduino/sensores_v3/sensores_v3.ino
@@ -7,46 +7,19 @@
 #define SONAR_NUM 12 /* NÚMERO DE SENSORES */
 #define MAX_DISTANCE 300 /* DISTÂNCIA MÁXIMA DE 200 CM */
 
-/* DECLARAÇÃO DE CONSTANTES COMEÇO */
-  #define echo_sensor01  22
-  #define trigger_sensor01  23
-  #define echo_sensor02  24
-  #define trigger_sensor02  25
-  #define echo_sensor03  26
-  #define trigger_sensor03  27
-  #define echo_sensor04  28
-  #define trigger_sensor04  29
-  #define echo_sensor05  30
-  #define trigger_sensor05  31
-  #define echo_sensor06  32
-  #define trigger_sensor06  33
-  #define echo_sensor07  34
-  #define trigger_sensor07  35
-  #define echo_sensor08  36
-  #define trigger_sensor08  37
-  #define echo_sensor09  38
-  #define trigger_sensor09  39
-  #define echo_sensor10  40
-  #define trigger_sensor10  41
-  #define echo_sensor11  42
-  #define trigger_sensor11  43
-  #define echo_sensor12  44
-  #define trigger_sensor12  45
-/* DECLARAÇÃO DE CONSTANTES COMEÇO */
-
 /* ENTRADA E SAÍDA DE SENSORES */
-  NewPing sensor01(trigger_sensor01, echo_sensor01, 200);
-  NewPing sensor02(trigger_sensor02, echo_sensor02, 200);
-  NewPing sensor03(trigger_sensor03, echo_sensor03, 200);
-  NewPing sensor04(trigger_sensor04, echo_sensor04, 200);
-  NewPing sensor05(trigger_sensor05, echo_sensor05, 200);
-  NewPing sensor06(trigger_sensor06, echo_sensor06, 200);
-  NewPing sensor07(trigger_sensor07, echo_sensor07, 200);
-  NewPing sensor08(trigger_sensor08, echo_sensor08, 200);
-  NewPing sensor09(trigger_sensor09, echo_sensor09, 200);
-  NewPing sensor10(trigger_sensor10, echo_sensor10, 200);
-  NewPing sensor11(trigger_sensor11, echo_sensor11, 200);
-  NewPing sensor12(trigger_sensor12, echo_sensor12, 200);
+  NewPing sensor01(23, 22, 200);
+  NewPing sensor02(25, 24, 200);
+  NewPing sensor03(27, 26, 200);
+  NewPing sensor04(29, 28, 200);
+  NewPing sensor05(31, 30, 200);
+  NewPing sensor06(33, 32, 200);
+  NewPing sensor07(35, 34, 200);
+  NewPing sensor08(37, 36, 200);
+  NewPing sensor09(39, 38, 200);
+  NewPing sensor10(41, 40, 200);
+  NewPing sensor11(43, 42, 200);
+  NewPing sensor12(45, 44, 200);
 /* ENTRADA E SAÍDA DE SENSORES FIM */
 
 void setup() {
