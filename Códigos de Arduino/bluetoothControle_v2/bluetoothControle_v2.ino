4a08ad147b3f8f4206ff42d90ae486ff383fae6b
JVHelion <45883508+JVHelion@users.noreply.github.com>
2023-11-08 11:16:03 -0300

    Notas e Código v2.7.1
    Tentativa de unificar todos os drivers em 1 arduino uno

---
+++ Códigos de Arduino/bluetoothControle_v2/bluetoothControle_v2.ino
@@ -0,0 +1,72 @@
+#define PUL_H 8
+#define DIR_H 9
+#define ENA_H 10 
+
+#define PUL_V 11
+#define DIR_V 12
+#define ENA_V 13
+
+#include <Dabble.h>
+
+void setup() {
+  Serial.begin(9600);
+  
+  pinMode(PUL_H, OUTPUT);
+  pinMode(DIR_H, OUTPUT);
+  pinMode(ENA_H, OUTPUT);
+  pinMode(PUL_V, OUTPUT);
+  pinMode(DIR_V, OUTPUT);
+  pinMode(ENA_V, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(ENA_H, LOW); // Habilita o driver
+  digitalWrite(ENA_V, LOW); // Habilita o driver
+  if (Serial.available()) {
+    char recebidoBluetooth = Serial.read();
+    Serial.println(recebidoBluetooth);
+    if (recebidoBluetooth == 'W') {
+      digitalWrite(DIR_H, HIGH); // Define a direção do motor
+      //Serial.println("W");
+      for (int i = 0; i < 200; i++) { // Gere pulsos para girar o motor
+        digitalWrite(PUL_H, HIGH);
+        delayMicroseconds(500); // Ajuste a velocidade aqui
+        digitalWrite(PUL_H, LOW);
+        delayMicroseconds(500);
+      }
+    }
+    else if (recebidoBluetooth == 'S'){
+      digitalWrite(DIR_H, LOW); // Define a direção do motor
+      //Serial.println("S");
+      for (int i = 0; i < 200; i++) { // Gere pulsos para girar o motor
+        digitalWrite(PUL_H, HIGH);
+        delayMicroseconds(500); // Ajuste a velocidade aqui
+        digitalWrite(PUL_H, LOW);
+        delayMicroseconds(500);
+      }
+      
+    }
+    else if (recebidoBluetooth == 'A'){
+      digitalWrite(DIR_V, LOW); // Define a direção do motor
+      //Serial.println("S");
+      for (int i = 0; i < 200; i++) { // Gere pulsos para girar o motor
+        digitalWrite(PUL_V, HIGH);
+        delayMicroseconds(500); // Ajuste a velocidade aqui
+        digitalWrite(PUL_V, LOW);
+        delayMicroseconds(500);
+      }
+      
+    }
+    else if (recebidoBluetooth == 'D'){
+      digitalWrite(DIR_V, LOW); // Define a direção do motor
+      //Serial.println("S");
+      for (int i = 0; i < 200; i++) { // Gere pulsos para girar o motor
+        digitalWrite(PUL_V, HIGH);
+        delayMicroseconds(500); // Ajuste a velocidade aqui
+        digitalWrite(PUL_V, LOW);
+        delayMicroseconds(500);
+      }
+      
+    }
+  }
+}
