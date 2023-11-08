0f31648660a02bf292237ab90599044cf246b5fa
JVHelion <45883508+JVHelion@users.noreply.github.com>
2023-11-08 09:52:42 -0300

    Notas e Código v2.7
    Capta Bluetooth com sucesso

--- Códigos de Arduino/bluetoothControle_v1/bluetoothControle_v1.ino
+++ Códigos de Arduino/bluetoothControle_v1/bluetoothControle_v1.ino
@@ -1,15 +1,12 @@
-#include <SoftwareSerial.h>
-
 #define PUL 8
 #define DIR 9
 #define ENA 10
 
-SoftwareSerial serial1(5, 6); /* RX, TX */
+#include <Dabble.h>
 
 void setup() {
-  Serial.begin(19200);
-  serial1.begin(4800);
-
+  Serial.begin(9600);
+  
   pinMode(PUL, OUTPUT);
   pinMode(DIR, OUTPUT);
   pinMode(ENA, OUTPUT);
@@ -20,16 +17,27 @@ void loop() {
   
   if (Serial.available()) {
     char recebidoBluetooth = Serial.read();
-    if (recebidoBluetooth = 'W') {
+    Serial.println(recebidoBluetooth);
+    if (recebidoBluetooth == 'W') {
       digitalWrite(DIR, HIGH); // Define a direção do motor
-      digitalWrite(PUL, HIGH);
       Serial.println("W");
-      }
-      else if (recebidoBluetooth == 'S'){
-      digitalWrite(DIR, LOW); // Define a direção do motor  
-      digitalWrite(PUL, LOW);
-      Serial.println("S");
+      for (int i = 0; i < 200; i++) { // Gere pulsos para girar o motor
+        digitalWrite(PUL, HIGH);
+        delayMicroseconds(500); // Ajuste a velocidade aqui
+        digitalWrite(PUL, LOW);
+        delayMicroseconds(500);
       }
     }
-
+    else if (recebidoBluetooth == 'S'){
+      digitalWrite(DIR, LOW); // Define a direção do motor
+      Serial.println("S");
+      for (int i = 0; i < 200; i++) { // Gere pulsos para girar o motor
+        digitalWrite(PUL, HIGH);
+        delayMicroseconds(500); // Ajuste a velocidade aqui
+        digitalWrite(PUL, LOW);
+        delayMicroseconds(500);
+      }
+      
+    }
+  }
 }
