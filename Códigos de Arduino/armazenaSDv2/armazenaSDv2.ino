d06a810b54bf7d576d6232cefb43796c1ed64834
JVHelion <45883508+JVHelion@users.noreply.github.com>
2023-11-17 10:59:26 -0300

    Notas e Código v2.8.1
    Removido a biblioteca softwareserial do armazenaSDv2

--- Códigos de Arduino/armazenaSDv2/armazenaSDv2.ino
+++ Códigos de Arduino/armazenaSDv2/armazenaSDv2.ino
@@ -1,18 +1,11 @@
-#include <SoftwareSerial.h>
-
-#define rxPin 10
-#define txPin 11
-
-SoftwareSerial serialCustom(rxPin, txPin);
-
 void setup() {
   Serial.begin(9600);
-  serialCustom.begin(9600);
+  Serial1.begin(9600);
 }
 
 void loop() {
-  if (serialCustom.available()) {
-    char recebido = serialCustom.read();
+  if (Serial1.available()) {
+    char recebido = Serial1.read();
     Serial.print("Recebido: ");
     Serial.println(recebido);
   }
