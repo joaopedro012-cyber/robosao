1dec2804a796f1fc777410c2693ee66eaafaeeed
robo_automacao <robo.automacao0001@gmail.com>
2024-01-22 08:30:10 -0300

    Notas e Código v3.3

--- platformio_codigos/GPS V2/src/main.cpp
+++ platformio_codigos/GPS V2/src/main.cpp
@@ -1,34 +1,122 @@
 #include <Arduino.h>
-#include <TinyGPS++.h>
 #include <SoftwareSerial.h>
+#include <TinyGPS.h>
 
-// Configura os pinos RX e TX
-static const int RXPin = 4, TXPin = 3;
-
-// Configura a velocidade de transmissão de dados (baud rate)
-static const uint32_t GPSBaud = 9600;
-
-// Configura o objeto TinyGPSPlus
-TinyGPSPlus gps;
-
-// Configura o objeto SoftwareSerial
-SoftwareSerial ss(RXPin, TXPin);
+SoftwareSerial serial1(10, 11); // RX, TX
+TinyGPS gps1;
 
 void setup() {
-  Serial.begin(9600);
-  ss.begin(GPSBaud);
+   serial1.begin(9600);
+   Serial.begin(9600);
+
+   Serial.println("O GPS do Brincando com Ideias aguarda pelo sinal dos satelites...");
 }
 
 void loop() {
-  // Verifica se há novos dados disponíveis no módulo GPS
-  while (ss.available() > 0) {
-    gps.encode(ss.read());
-    if (gps.location.isUpdated()) {
-      // Imprime as coordenadas de latitude e longitude
-      Serial.print("Latitude= "); 
-      Serial.print(gps.location.lat(), 6);
-      Serial.print(" Longitude= "); 
-      Serial.println(gps.location.lng(), 6);
-    }
+  bool recebido = false;
+
+  while (serial1.available()) {
+     char cIn = serial1.read();
+     recebido = gps1.encode(cIn);
+  }
+
+  if (recebido) {
+     Serial.println("----------------------------------------");
+     
+     //Latitude e Longitude
+     long latitude, longitude;
+     unsigned long idadeInfo;
+     gps1.get_position(&latitude, &longitude, &idadeInfo);     
+
+     if (latitude != TinyGPS::GPS_INVALID_F_ANGLE) {
+        Serial.print("Latitude: ");
+        Serial.println(float(latitude) / 100000, 6);
+     }
+
+     if (longitude != TinyGPS::GPS_INVALID_F_ANGLE) {
+        Serial.print("Longitude: ");
+        Serial.println(float(longitude) / 100000, 6);
+     }
+
+     if (idadeInfo != TinyGPS::GPS_INVALID_AGE) {
+        Serial.print("Idade da Informacao (ms): ");
+        Serial.println(idadeInfo);
+     }
+
+
+     //Dia e Hora
+     int ano;
+     byte mes, dia, hora, minuto, segundo, centesimo;
+     gps1.crack_datetime(&ano, &mes, &dia, &hora, &minuto, &segundo, &centesimo, &idadeInfo);
+
+     Serial.print("Data (GMT): ");
+     Serial.print(dia);
+     Serial.print("/");
+     Serial.print(mes);
+     Serial.print("/");
+     Serial.println(ano);
+
+     Serial.print("Horario (GMT): ");
+     Serial.print(hora);
+     Serial.print(":");
+     Serial.print(minuto);
+     Serial.print(":");
+     Serial.print(segundo);
+     Serial.print(":");
+     Serial.println(centesimo);
+
+
+     //altitude
+     float altitudeGPS;
+     altitudeGPS = gps1.f_altitude();
+
+     if ((altitudeGPS != TinyGPS::GPS_INVALID_ALTITUDE) && (altitudeGPS != 1000000)) {
+        Serial.print("Altitude (cm): ");
+        Serial.println(altitudeGPS);
+     }
+
+
+     //velocidade
+     float velocidade;
+     //velocidade = gps1.speed();        //nós
+     velocidade = gps1.f_speed_kmph();   //km/h
+     //velocidade = gps1.f_speed_mph();  //milha/h
+     //velocidade = gps1.f_speed_mps();  //milha/segundo
+
+     Serial.print("Velocidade (km/h): ");
+     Serial.println(velocidade, 2);  //Conversão de Nós para Km/h
+
+
+
+     //sentito (em centesima de graus)
+     unsigned long sentido;
+     sentido = gps1.course();
+
+     Serial.print("Sentido (grau): ");
+     Serial.println(float(sentido) / 100, 2);
+
+
+     //satelites e precisão
+     unsigned short satelites;
+     unsigned long precisao;
+     satelites = gps1.satellites();
+     precisao =  gps1.hdop();
+
+     if (satelites != TinyGPS::GPS_INVALID_SATELLITES) {
+        Serial.print("Satelites: ");
+        Serial.println(satelites);
+     }
+
+     if (precisao != TinyGPS::GPS_INVALID_HDOP) {
+        Serial.print("Precisao (centesimos de segundo): ");
+        Serial.println(precisao);
+     }
+
+
+     //float distancia_entre;
+     //distancia_entre = gps1.distance_between(lat1, long1, lat2, long2);
+
+     //float sentido_para;
+     //sentido_para = gps1.course_to(lat1, long1, lat2, long2);
   }
 }
---
+++ platformio_codigos/recebeBluetooth/src/main.cpp
@@ -0,0 +1,87 @@
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+
+#define rxPin A0
+#define txPin A1
+
+#define PUL_H 8
+#define DIR_H 9
+#define ENA_H 10 
+
+#define PUL_V 11
+#define DIR_V 12
+#define ENA_V 13
+
+SoftwareSerial serialCustom(rxPin, txPin);
+
+void setup() {
+  serialCustom.begin(9600);
+  Serial.begin(9600);
+  
+  pinMode(PUL_H, OUTPUT);
+  pinMode(DIR_H, OUTPUT);
+  pinMode(ENA_H, OUTPUT);
+  pinMode(PUL_V, OUTPUT);
+  pinMode(DIR_V, OUTPUT);
+  pinMode(ENA_V, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(ENA_H, LOW); // Habilita o driver
+  digitalWrite(ENA_V, LOW); // Habilita o driver
+  if (serialCustom.available()) {
+    char recebidoBluetooth = serialCustom.read();
+    //serialCustom.println(recebidoBluetooth);
+    if (recebidoBluetooth == 'W') {
+      Serial.write('W'); 
+      serialCustom.print('w');
+      delay(10);
+      digitalWrite(DIR_H, HIGH); // Define a direção do motor
+      for (int i = 0; i < 400; i++) { // Gere pulsos para girar o motor
+        digitalWrite(PUL_H, HIGH);
+        delayMicroseconds(500); // Ajuste a velocidade aqui
+        digitalWrite(PUL_H, LOW);
+        delayMicroseconds(500);
+      }
+    }
+    else if (recebidoBluetooth == 'S'){
+      Serial.write('S');
+      serialCustom.print('s');
+      delay(10);
+      digitalWrite(DIR_H, LOW); // Define a direção do motor
+      for (int i = 0; i < 1000; i++) { // Gere pulsos para girar o motor
+        digitalWrite(PUL_H, HIGH);
+        delayMicroseconds(500); // Ajuste a velocidade aqui
+        digitalWrite(PUL_H, LOW);
+        delayMicroseconds(500);
+      }
+      
+    }
+    else if (recebidoBluetooth == 'A'){
+      Serial.write('A'); 
+      serialCustom.print('a');
+      delay(10);
+      digitalWrite(DIR_V, LOW); // Define a direção do motor
+      for (int i = 0; i < 400; i++) { // Gere pulsos para girar o motor
+        digitalWrite(PUL_V, HIGH);
+        delayMicroseconds(500); // Ajuste a velocidade aqui
+        digitalWrite(PUL_V, LOW);
+        delayMicroseconds(500);
+      }
+      
+    }
+    else if (recebidoBluetooth == 'D'){
+      Serial.write('D');
+      serialCustom.print('d');
+      delay(10);
+      digitalWrite(DIR_V, HIGH); // Define a direção do motor
+      for (int i = 0; i < 1000; i++) { // Gere pulsos para girar o motor
+        digitalWrite(PUL_V, HIGH);
+        delayMicroseconds(500); // Ajuste a velocidade aqui
+        digitalWrite(PUL_V, LOW);
+        delayMicroseconds(500);
+      }
+      
+    }
+  }
+}
