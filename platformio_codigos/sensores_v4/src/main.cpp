81aad94e617d73e8c793e94c32767033b65bef9b
roboAutoomacao <robo.automacao0001@gmail.com>
2024-06-26 09:09:15 -0300

    sensores_v4 agora cuida dos sensores físicos

--- platformio_codigos/sensores_v4/src/main.cpp
+++ platformio_codigos/sensores_v4/src/main.cpp
@@ -1,10 +1,11 @@
 /* BIBLIOTECAS COMEÇO */
 #include <Arduino.h>
 #include <NewPing.h>
+#include <stdio.h>
 /* BIBLIOTECAS FINAL */
 
-#define SONAR_NUM 12     /* NÚMERO DE SENSORES */
-#define MAX_DISTANCE 200 /* DISTÂNCIA MÁXIMA DE 200 CM */
+#define SONAR_NUM 12     /* NÚMERO DE SENSORES ULTRASSOM */
+#define MAX_DISTANCE 200 /* DISTÂNCIA MÁXIMA DE 200 CM NOS SENSORES DE ULTRASSOM */
 
 /* ENTRADA E SAÍDA DE SENSORES COMEÇO */
 NewPing sensor01(23, 22, MAX_DISTANCE);
@@ -27,21 +28,62 @@ void setup()
   Serial1.begin(9600);
 }
 
+void sensorUltrassom(int portaSensorUltrassom)
+{
+}
+
+void sensorFisico(int portaSensorFisico)
+{
+  pinMode(portaSensorFisico, INPUT_PULLUP);
+  char str[10]; // Aumente o tamanho conforme necessário
+  int sensorValue = digitalRead(portaSensorFisico);
+
+  if (sensorValue == HIGH)
+  {
+    snprintf(str, sizeof(str), "b%d", portaSensorFisico);
+  }
+  else
+  {
+    snprintf(str, sizeof(str), "A%d", portaSensorFisico);
+  }
+
+  Serial.println(str);
+}
+
 void loop()
 {
-      /* Serial.write(sensor01.ping_cm());
-      Serial.write(sensor02.ping_cm());
-      Serial.write(sensor03.ping_cm());
-      Serial.write(sensor04.ping_cm());
-      Serial.write(sensor05.ping_cm());
-      Serial.write(sensor06.ping_cm());
-      Serial.write(sensor07.ping_cm());
-      Serial.write(sensor08.ping_cm());
-      Serial.write(sensor09.ping_cm());
-      Serial.write(sensor10.ping_cm());
-      Serial.write(sensor11.ping_cm());
-      Serial.write(sensor12.ping_cm());
-       */
-      delay (500)
-    ;
+  sensorFisico(2);
+  sensorFisico(3);
+  sensorFisico(4);
+  sensorFisico(5);
+  sensorFisico(6);
+  sensorFisico(7);
+  sensorFisico(8);
+  sensorFisico(9);
+  Serial.print("S01:");
+  Serial.println(sensor01.ping_cm());
+  Serial.print("S02:");
+  Serial.println(sensor02.ping_cm());
+  Serial.print("S03:");
+  Serial.println(sensor03.ping_cm());
+  Serial.print("S04:");
+  Serial.println(sensor04.ping_cm());
+  Serial.print("S05:");
+  Serial.println(sensor05.ping_cm());
+  Serial.print("S06:");
+  Serial.println(sensor06.ping_cm());
+  Serial.print("S07:");
+  Serial.println(sensor07.ping_cm());
+  Serial.print("S08:");
+  Serial.println(sensor08.ping_cm());
+  Serial.print("S09:");
+  Serial.println(sensor09.ping_cm());
+  Serial.print("S10:");
+  Serial.println(sensor10.ping_cm());
+  Serial.print("S11:");
+  Serial.println(sensor11.ping_cm());
+  Serial.print("S12:");
+  Serial.println(sensor12.ping_cm());
+
+  delay(900);
 }
