9fba57b28351c98396b169ed3269dc4c005c27b2
roboAutoomacao <robo.automacao0001@gmail.com>
2024-07-17 11:02:36 -0300

    Alerado Arduinos

--- platformio_codigos/motores_horizontal/src/main.cpp
+++ platformio_codigos/motores_horizontal/src/main.cpp
@@ -6,8 +6,12 @@
 #define PUL_H 12
 #define DIR_H 11
 
-SoftwareSerial serialfisico(10, 9);     // RX, TX
-SoftwareSerial serialbluetooth(A1, A0); // RX, TX
+// Define os pinos para RX e TX
+const int rxPin = A0;
+const int txPin = A1;
+
+// Cria uma instância da classe SoftwareSerial
+SoftwareSerial serialbluetooth(rxPin, txPin); // RX, TX
 
 #define delayMs 45
 #define pulsosHoriz 300
@@ -18,36 +22,42 @@ void setup()
    pinMode(DIR_H, OUTPUT);
    pinMode(PUL_H, OUTPUT);
    serialbluetooth.begin(9600);
-   serialfisico.begin(9600);
    Serial.begin(9600);
 }
 
 void loop()
 {
+   char recebidoMonitorSerial = 0;
+
    if (Serial.available())
    {
-      char recebidoMonitorSerial = serialfisico.read() || serialbluetooth.read();
-      if (recebidoMonitorSerial == 'x' || recebidoMonitorSerial == 'X')
-      {
-         digitalWrite(DIR_H, HIGH);
-         for (int h = 0; h < pulsosHoriz; h++)
-         { // Gere pulsosHoriz    para girar o motor
-            digitalWrite(PUL_H, HIGH);
-            delayMicroseconds(delayMs); // Ajuste a velocidade aqui
-            digitalWrite(PUL_H, LOW);
-            delayMicroseconds(delayMs);
-         }
+      recebidoMonitorSerial = Serial.read();
+   }
+   else if (serialbluetooth.available())
+   {
+      recebidoMonitorSerial = serialbluetooth.read();
+   }
+
+   if (recebidoMonitorSerial == 'x' || recebidoMonitorSerial == 'X')
+   {
+      digitalWrite(DIR_H, HIGH);
+      for (int h = 0; h < pulsosHoriz; h++)
+      { // Gere pulsosHoriz para girar o motor
+         digitalWrite(PUL_H, HIGH);
+         delayMicroseconds(delayMs); // Ajuste a velocidade aqui
+         digitalWrite(PUL_H, LOW);
+         delayMicroseconds(delayMs);
       }
-      if (recebidoMonitorSerial == 'w' || recebidoMonitorSerial == 'W')
-      {
-         digitalWrite(DIR_H, LOW);
-         for (int r = 0; r < pulsosHoriz; r++)
-         { // Gere pulsosHoriz para girar o motor
-            digitalWrite(PUL_H, HIGH);
-            delayMicroseconds(delayMs); // Ajuste a velocidade aqui
-            digitalWrite(PUL_H, LOW);
-            delayMicroseconds(delayMs);
-         }
+   }
+   else if (recebidoMonitorSerial == 'w' || recebidoMonitorSerial == 'W')
+   {
+      digitalWrite(DIR_H, LOW);
+      for (int r = 0; r < pulsosHoriz; r++)
+      { // Gere pulsosHoriz para girar o motor
+         digitalWrite(PUL_H, HIGH);
+         delayMicroseconds(delayMs); // Ajuste a velocidade aqui
+         digitalWrite(PUL_H, LOW);
+         delayMicroseconds(delayMs);
       }
    }
 }
--- platformio_codigos/motores_vertical/src/main.cpp
+++ platformio_codigos/motores_vertical/src/main.cpp
@@ -6,8 +6,12 @@
 #define PUL_V 12
 #define DIR_V 11
 
-SoftwareSerial serialfisico(10, 9);     // RX, TX
-SoftwareSerial serialbluetooth(A1, A0); // RX, TX
+// Define os pinos para RX e TX
+const int rxPin = A0;
+const int txPin = A1;
+
+// Cria uma instância da classe SoftwareSerial
+SoftwareSerial serialbluetooth(rxPin, txPin); // RX, TX
 
 #define delayMs 45
 #define pulsosVert 300
@@ -18,37 +22,42 @@ void setup()
    pinMode(DIR_V, OUTPUT);
    pinMode(PUL_V, OUTPUT);
    serialbluetooth.begin(9600);
-   serialfisico.begin(9600);
    Serial.begin(9600);
 }
 
 void loop()
 {
+   char recebidoMonitorSerial = 0;
+
    if (Serial.available())
    {
-      char recebidoMonitorSerial = Serial.read();
+      recebidoMonitorSerial = Serial.read();
+   }
+   else if (serialbluetooth.available())
+   {
+      recebidoMonitorSerial = serialbluetooth.read();
+   }
 
-      if (recebidoMonitorSerial == 'a' || recebidoMonitorSerial == 'A')
-      {
-         digitalWrite(DIR_V, LOW);
-         for (int b = 0; b < pulsosVert; b++)
-         { // Gere pulsosVert para girar o motor
-            digitalWrite(PUL_V, HIGH);
-            delayMicroseconds(delayMs); // Ajuste a velocidade aqui
-            digitalWrite(PUL_V, LOW);
-            delayMicroseconds(delayMs);
-         }
+   if (recebidoMonitorSerial == 'a' || recebidoMonitorSerial == 'A')
+   {
+      digitalWrite(DIR_V, HIGH);
+      for (int h = 0; h < pulsosVert; h++)
+      { // Gere pulsosHoriz para girar o motor
+         digitalWrite(PUL_V, HIGH);
+         delayMicroseconds(delayMs); // Ajuste a velocidade aqui
+         digitalWrite(PUL_V, LOW);
+         delayMicroseconds(delayMs);
       }
-      if (recebidoMonitorSerial == 'd' || recebidoMonitorSerial == 'D')
-      {
-         digitalWrite(DIR_V, HIGH);
-         for (int v = 0; v < pulsosVert; v++)
-         { // Gere pulsosVert para girar o motor
-            digitalWrite(PUL_V, HIGH);
-            delayMicroseconds(delayMs); // Ajuste a velocidade aqui
-            digitalWrite(PUL_V, LOW);
-            delayMicroseconds(delayMs);
-         }
+   }
+   else if (recebidoMonitorSerial == 'd' || recebidoMonitorSerial == 'D')
+   {
+      digitalWrite(DIR_V, LOW);
+      for (int r = 0; r < pulsosVert; r++)
+      { // Gere pulsosHoriz para girar o motor
+         digitalWrite(PUL_V, HIGH);
+         delayMicroseconds(delayMs); // Ajuste a velocidade aqui
+         digitalWrite(PUL_V, LOW);
+         delayMicroseconds(delayMs);
       }
    }
 }
