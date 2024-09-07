01e5ab5628147b5617b2c5201fa48a12c8eca216
roboAutoomacao <152507142+roboAutoomacao@users.noreply.github.com>
2024-09-07 10:16:03 -0300

    pulsos

--- platformio_codigos/motores_vertical/src/main.cpp
+++ platformio_codigos/motores_vertical/src/main.cpp
@@ -14,7 +14,7 @@ const int txPin = A1;
 SoftwareSerial serialbluetooth(rxPin, txPin); // RX, TX
 
 #define delayMs 45
-#define pulsosVert 6400
+#define pulsosVert 1000
 #define pulsosDiagonais 3200
 
 void setup()
