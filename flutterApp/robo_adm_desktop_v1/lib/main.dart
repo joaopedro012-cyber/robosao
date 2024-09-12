12211221d8c4b366649cf45db379b5daab24e31f
Joao.Helion <joaovictorheliondeoliveira@gmail.com>
2024-09-12 11:02:43 -0300

    BASE de desenvolvimento app

--- flutterApp/robo_adm_desktop_v1/lib/main.dart
+++ flutterApp/robo_adm_desktop_v1/lib/main.dart
@@ -1,124 +1,125 @@
-import 'dart:ui';
-
-import 'package:flutter/foundation.dart';
-import 'package:window_manager/window_manager.dart';
+import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter/material.dart';
+import 'package:window_manager/window_manager.dart';
 import 'package:flutter_acrylic/flutter_acrylic.dart' as flutter_acrylic;
-import 'src/utils/util.dart';
-import 'src/themes/theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
+
   await flutter_acrylic.Window.initialize();
-  if (defaultTargetPlatform == TargetPlatform.windows) {
-    await flutter_acrylic.Window.hideWindowControls();
-  }
   await WindowManager.instance.ensureInitialized();
-  windowManager.waitUntilReadyToShow().then((_) async {
-    await windowManager.setTitleBarStyle(TitleBarStyle.hidden, windowButtonVisibility: false,);
-  await windowManager.setMinimumSize(const Size(500, 600));
-  await windowManager.show();
-  await windowManager.setPreventClose(true);
-  
-  },);
+  windowManager.waitUntilReadyToShow().then(
+    (_) async {
+      await windowManager.setMinimumSize(const Size(200, 300));
+      await windowManager.show();
+      await windowManager.setPreventClose(false);
+      await windowManager.setSkipTaskbar(false);
+    },
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final brightness = View.of(context).platformDispatcher.platformBrightness;
-
-    // Retrieves the default theme for the platform
-    //TextTheme textTheme = Theme.of(context).textTheme;
-
-    // Use with Google Fonts package to use downloadable fonts
-    TextTheme textTheme = createTextTheme(context, "Cairo", "Atkinson Hyperlegible");
-
-    MaterialTheme theme = MaterialTheme(textTheme);
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: brightness == Brightness.light ? theme.light() : theme.dark(),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return FluentApp(
+      title: 'Navegação',
+      theme: FluentThemeData(),
+      home: const MyNavigationView(), // Use o novo nome da classe aqui
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class MyNavigationView extends StatefulWidget { // Renomeado de NavigationView para MyNavigationView
+  const MyNavigationView({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyNavigationView> createState() => _MyNavigationViewState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyNavigationViewState extends State<MyNavigationView> {
+  int topIndex = 0;
+  PaneDisplayMode displayMode = PaneDisplayMode.compact;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  List<NavigationPaneItem> items = [
+    PaneItem(
+      icon: const Icon(FluentIcons.home),
+      title: const Text('Inicio'),
+      body: const Center(child: Text('Inicio')),
+    ),
+    PaneItemSeparator(),
+    PaneItem(
+      icon: const Icon(FluentIcons.issue_tracking),
+      title: const Text('Track Orders'),
+      infoBadge: const InfoBadge(source: Text('8')),
+      body: const Center(
+        child: Text('Order tracking information here.'),
+      ),
+    ),
+    PaneItem(
+      icon: const Icon(FluentIcons.disable_updates),
+      title: const Text('Disabled Item'),
+      body: const Center(child: Text('This item is disabled')),
+      enabled: false,
+    ),
+    PaneItemExpander(
+      icon: const Icon(FluentIcons.account_management),
+      title: const Text('Account'),
+      body: const Center(child: Text('Account Information')),
+      items: [
+        PaneItem(
+          icon: const Icon(FluentIcons.mail),
+          title: const Text('Mail'),
+          body: const Center(child: Text('Mail Page')),
+        ),
+        PaneItem(
+          icon: const Icon(FluentIcons.calendar),
+          title: const Text('Calendar'),
+          body: const Center(child: Text('Calendar Page')),
+        ),
+      ],
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: NavigationView( // Use o widget NavigationView do Fluent UI aqui
+        appBar: const NavigationAppBar(
+          title: Text('NavigationView Example'),
+        ),
+        pane: NavigationPane(
+          selected: topIndex,
+          onChanged: (index) => setState(() => topIndex = index),
+          displayMode: displayMode,
+          items: items,
+          footerItems: [
+            PaneItem(
+              icon: const Icon(FluentIcons.settings),
+              title: const Text('Settings'),
+              body: const Center(child: Text('Settings Page')),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            PaneItemAction(
+              icon: const Icon(FluentIcons.add),
+              title: const Text('Add New Item'),
+              onTap: () {
+                // Lógica para adicionar novo item
+                items.add(
+                  PaneItem(
+                    icon: const Icon(FluentIcons.new_folder),
+                    title: const Text('New Item'),
+                    body:
+                        const Center(child: Text('This is a newly added item')),
+                  ),
+                );
+                setState(() {});
+              },
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
-
