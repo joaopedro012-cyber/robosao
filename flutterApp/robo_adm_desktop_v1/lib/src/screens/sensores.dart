f56ec07d3e131b6684f64ba5a0a03ac6beb249e4
JVHelion <joaovictorheliondeoliveira@gmail.com>
2024-09-27 01:22:25 -0300

    agora puxa arquivos do diretório

--- flutterApp/robo_adm_desktop_v1/lib/src/screens/sensores.dart
+++ flutterApp/robo_adm_desktop_v1/lib/src/screens/sensores.dart
@@ -1,5 +1,7 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:fluent_ui/fluent_ui.dart' as fui;
+import 'package:path_provider/path_provider.dart';
 
 class SensoresPage extends StatefulWidget {
   const SensoresPage({super.key});
@@ -9,48 +11,73 @@ class SensoresPage extends StatefulWidget {
 }
 
 class _SensoresPageState extends State<SensoresPage> {
+  String? selected;
+  late Future<List<String>> rotinasNoDiretorio;
+
+  @override
+  void initState() {
+    super.initState();
+    rotinasNoDiretorio = listarArquivosJson();
+  }
+
+  Future<List<String>> listarArquivosJson() async {
+    final Directory diretorio = await getApplicationDocumentsDirectory();
+    final String diretorioFinalCaminho = '${diretorio.path}/Rotinas Robo';
+    final Directory diretorioFinal = Directory(diretorioFinalCaminho);
+    final List<FileSystemEntity> arquivos = diretorioFinal.listSync();
+
+    return arquivos.where((file) => file is File).map((file) => file.path).toList();
+  }
+
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
-    String? selected;
-    const rotinasNoDiretorio = <String>[
-      'Abyssinian',
-      'Aegean',
-      'American Bobtail',
-      'American Curl',
-    ];
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        SizedBox(
-          width: screenWidth * 0.35,
-          child: Wrap(
+
+    return FutureBuilder<List<String>>(
+      future: rotinasNoDiretorio,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return CircularProgressIndicator();
+        } else if (snapshot.hasError) {
+          return Text('Erro ao carregar arquivos');
+        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          return Text('Nenhum arquivo encontrado');
+        } else {
+          List<String> rotinas = snapshot.data!;
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               SizedBox(
-                width: screenWidth * 0.27,
-                children: [
-                  fui.AutoSuggestBox<String>(
-                    placeholder: 'Rotinas',
-                    items: rotinasNoDiretorio.map((cat) {
-                      return fui.AutoSuggestBoxItem<String>(
-                          value: cat,
-                          label: cat,
-                          onFocusChange: (focused) {
-                            if (focused) {
-                              debugPrint('Focused $cat');
-                            }
-                          });
-                    }).toList(),
-                    onSelected: (item) {
-                      setState(() => selected = item.value);
-                    },
-                  ),
-                ],
-              )
+                width: screenWidth * 0.35,
+                child: Wrap(
+                  children: [
+                    SizedBox(
+                      width: screenWidth * 0.27,
+                      child: fui.AutoSuggestBox<String>(
+                        placeholder: 'Rotinas',
+                        items: rotinas.map((cat) {
+                          return fui.AutoSuggestBoxItem<String>(
+                            value: cat,
+                            label: cat,
+                            onFocusChange: (focused) {
+                              if (focused) {
+                                debugPrint('Focused $cat');
+                              }
+                            },
+                          );
+                        }).toList(),
+                        onSelected: (item) {
+                          setState(() => selected = item.value);
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
-          ),
-        )
-      ],
+          );
+        }
+      },
     );
   }
 }
