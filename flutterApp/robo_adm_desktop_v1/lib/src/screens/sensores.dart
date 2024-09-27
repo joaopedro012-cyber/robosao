e3c0c47a55e38786b1f090c104245fe83e612bdf
Joao.Helion <joaovictorheliondeoliveira@gmail.com>
2024-09-27 09:04:44 -0300

    Modelo de rotina no sensor

--- flutterApp/robo_adm_desktop_v1/lib/src/screens/sensores.dart
+++ flutterApp/robo_adm_desktop_v1/lib/src/screens/sensores.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:fluent_ui/fluent_ui.dart' as fui;
+import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
 
 class SensoresPage extends StatefulWidget {
@@ -25,28 +26,39 @@ class _SensoresPageState extends State<SensoresPage> {
     final String diretorioFinalCaminho = '${diretorio.path}/Rotinas Robo';
     final Directory diretorioFinal = Directory(diretorioFinalCaminho);
     final List<FileSystemEntity> arquivos = diretorioFinal.listSync();
+    double screenWidth = MediaQuery.of(context).size.width;
 
-    return arquivos.where((file) => file is File).map((file) => file.path).toList();
+    return arquivos
+        .whereType<File>()
+        .map((file) => p.basename(file.path))
+        .toList();
   }
 
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
+    int? numberBoxValue = 100;
+    void _valueChanged(int? newValue) {
+      setState(() {
+        numberBoxValue = newValue;
+      });
+    }
 
     return FutureBuilder<List<String>>(
       future: rotinasNoDiretorio,
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return CircularProgressIndicator();
+          return const CircularProgressIndicator();
         } else if (snapshot.hasError) {
-          return Text('Erro ao carregar arquivos');
+          return const Text('Erro ao carregar arquivos');
         } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-          return Text('Nenhum arquivo encontrado');
+          return const Text('Nenhum arquivo encontrado');
         } else {
           List<String> rotinas = snapshot.data!;
-          return Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+          return Wrap(
+            //mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
+              const Text('Sensor 1'),
               SizedBox(
                 width: screenWidth * 0.35,
                 child: Wrap(
@@ -54,14 +66,14 @@ class _SensoresPageState extends State<SensoresPage> {
                     SizedBox(
                       width: screenWidth * 0.27,
                       child: fui.AutoSuggestBox<String>(
-                        placeholder: 'Rotinas',
-                        items: rotinas.map((cat) {
+                        placeholder: 'Exemplo.json',
+                        items: rotinas.map((rotina) {
                           return fui.AutoSuggestBoxItem<String>(
-                            value: cat,
-                            label: cat,
+                            value: rotina,
+                            label: rotina,
                             onFocusChange: (focused) {
                               if (focused) {
-                                debugPrint('Focused $cat');
+                                debugPrint('Focused $rotina');
                               }
                             },
                           );
@@ -74,6 +86,15 @@ class _SensoresPageState extends State<SensoresPage> {
                   ],
                 ),
               ),
+              Container(
+                width: screenWidth * 0.20,
+                child: fui.NumberBox(
+                  value: numberBoxValue,
+                  onChanged: _valueChanged,
+                  mode: fui.SpinButtonPlacementMode.inline,
+                ),
+              ),
+              const Text('cm'),
             ],
           );
         }
